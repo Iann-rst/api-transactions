c37947bed1ad3295c4c7737db3e78f29b0e7c998
Iann Rodrigues <iannrst11@gmail.com>
2023-02-06 19:03:10 -0300

    feat: rota para criar uma transaction

---
+++ src/routes/transactions.ts
@@ -0,0 +1,26 @@
+import { FastifyInstance } from 'fastify'
+import crypto from 'node:crypto'
+import { knex } from '../database'
+import { z } from 'zod'
+
+export async function transactionsRoute(app: FastifyInstance) {
+  app.post('/', async (request, response) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return response.status(201).send()
+  })
+}
