581e4984a2ecc021ad9722f1f10c96e0b9adfa5c
Iann Rodrigues <iannrst11@gmail.com>
2023-02-06 18:07:40 -0300

    feat: servidor

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import fastify from 'fastify'
+import { knex } from './database'
+import { env } from './env'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  const transaction = await knex('transactions').where('amount', 10).select('*')
+
+  return transaction
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
