a5aeddef6305c012db2366e479a235299f38b4bf
Iann Rodrigues <iannrst11@gmail.com>
2023-02-08 15:42:42 -0300

    :test_tube: test: Buscar o resumo das transações

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -90,4 +90,34 @@ describe('Transactions routes', () => {
       }),
     )
   })
+
+  it('should be able to get the summary', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Credit Transaction',
+        amount: 3500,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies)
+      .send({
+        title: 'New Transaction',
+        amount: 1500,
+        type: 'debit',
+      })
+
+    const summaryResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(summaryResponse.body.summary).toEqual({
+      amount: 2000,
+    })
+  })
 })
