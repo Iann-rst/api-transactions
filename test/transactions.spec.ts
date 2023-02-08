92714cf6c99de9a09eecfa5753fd019b0eae5f4b
Iann Rodrigues <iannrst11@gmail.com>
2023-02-08 12:31:59 -0300

    :test_tube: test: Listar as transações

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { beforeAll, afterAll, describe, it } from 'vitest'
+import { beforeAll, afterAll, describe, it, expect } from 'vitest'
 import { app } from '../src/app'
 import request from 'supertest'
 
@@ -20,5 +20,26 @@ describe('Transactions routes', () => {
         type: 'credit',
       })
       .expect(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 3500,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({ title: 'New Transaction', amount: 3500 }),
+    ])
   })
 })
