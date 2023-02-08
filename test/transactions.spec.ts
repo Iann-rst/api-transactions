aee25255d030e520ee4e452260002b05aa9d67cd
Iann Rodrigues <iannrst11@gmail.com>
2023-02-08 12:17:09 -0300

    :test_tube: test: teste criar uma nova transação

---
+++ test/transactions.spec.ts
@@ -0,0 +1,24 @@
+import { beforeAll, afterAll, describe, it } from 'vitest'
+import { app } from '../src/app'
+import request from 'supertest'
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 3500,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+})
